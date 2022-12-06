d991b661ec6e843982a1281f73679e7ac5e1bc18
Ashleigh3131 <brennan-A20@ulster.ac.uk>
2022-12-06 13:34:43 +0000

    Moving the files around for login to be index

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function validate(event) {
         window.location.href = "https://ambitious-field-04a184b03.2.azurestaticapps.net/";
     } else if (username == ["username1"] && password == ["password1"]) {
         alert("Login Successful");
-        window.location.href = "https://sandrino.dev/blog/configuring-azure-app-service-oidc-auth0";
+        window.location.href = "https://ambitious-field-04a184b03.2.azurestaticapps.net/consumerindex.html";
     } else {
         alert(
         "Sorry, your username or password was incorrect. Please double-check your login information and try again."
